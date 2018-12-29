ae93a597713db699e61134bbeb0a0f085d8b3365
zhoujiaojiao <229822087@qq.com>
2018-12-29 12:53:31 +0800

    zjj.models

--- careertest/zjj/models.py
+++ careertest/zjj/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from qsj.models import Company,Moding
 
 # Create your models here.
 class Job(models.Model):
     jobname=models.CharField(max_length=100)
-    company_id=models.ForeignKey(to='Company',on_delete=models.CASCADE,default=1)
-    moding_id=models.ForeignKey(to='Moding',on_delete=models.CASCADE)
+    company_id=models.ForeignKey(to=Company,on_delete=models.CASCADE,default=1)
+    moding_id=models.ForeignKey(to=Moding,on_delete=models.CASCADE)
     isdelete=models.BooleanField(default=False)
 class Jobneed(models.Model):
     job_id=models.ForeignKey(to='Job',on_delete=models.CASCADE)
     jobdescription=models.CharField(max_length=1024)
     jobrequirements=models.CharField(max_length=1024)
-    moding_id=models.ForeignKey(to='Moding',on_delete=models.CASCADE)
+    moding_id=models.ForeignKey(to=Moding,on_delete=models.CASCADE)
     isdelete=models.BooleanField(default=False)
 class Tester(models.Model):
     name=models.CharField(max_length=20)
